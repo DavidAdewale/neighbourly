9236a128b252f6c3adea2216ddfd538a8a508224
David <adewaledavidola@gmail.com>
2023-07-28 16:56:00 +0100

    debugging the hero-section glitch on vercel - 2

--- src/ui/HeroSection.jsx
+++ src/ui/HeroSection.jsx
@@ -2,8 +2,8 @@ import { styled } from 'styled-components';
 import HeroContent from './HeroContent';
 
 const StyledBackground = styled.main`
-  height: 100dvh;
-  /* background: var(--color-hero-bg); */
+  height: 100vh;
+  background: var(--color-hero-bg);
   background-blend-mode: overlay;
   background-size: cover;
   background-position: top center;
