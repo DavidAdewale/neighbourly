f15be56b7242870a622fbe328922bdd2460caf18
David <adewaledavidola@gmail.com>
2023-08-12 10:27:50 +0100

    fixing the scroll to top

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { useScrollToTop } from '../hooks/useScrollToTop';
 import CTASection from '../ui/CTASection';
 import FeaturesSection from '../ui/FeaturesSection';
 import HeroSection from '../ui/HeroSection';
 import NavBar from '../ui/NavBar';
 
 function Home() {
+  useScrollToTop();
   return (
     <>
       <NavBar />
