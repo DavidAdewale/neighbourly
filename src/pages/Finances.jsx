e4474158c43d583c54331e238f2345d0572ee4b8
David <adewaledavidola@gmail.com>
2023-08-20 14:53:53 +0100

    initialized a new feature called finances

--- src/pages/Finances.jsx
+++ src/pages/Finances.jsx
@@ -1,5 +1,14 @@
+import AppPage from '../ui/AppPage';
+import AppPageTitle from '../ui/AppPageTitle';
+
 function Finances() {
-  return <div>Finances</div>;
+  return (
+    <AppPage>
+      <AppPageTitle>
+        <h3>Finances</h3>
+      </AppPageTitle>
+    </AppPage>
+  );
 }
 
 export default Finances;
